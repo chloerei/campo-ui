08d06fabf3380e5892b47bbb1bc7caf5a9e77af2
Rei <chloerei@gmail.com>
2014-07-30 23:14:02 +0800

    Automated commit at 2014-07-30 15:14:02 UTC by middleman-deploy 0.2.4

---
+++ javascripts/application.js
@@ -0,0 +1,14 @@
+(function() {
+  $(document).on('click', '.left-nav .left-nav-toggle', function() {
+    $(this).closest('.left-nav').addClass('active');
+    return $('body').addClass('noscroll');
+  }).on('click', '.left-nav .left-nav-mask', function() {
+    $(this).closest('.left-nav').removeClass('active');
+    return $('body').removeClass('noscroll');
+  });
+
+}).call(this);
+(function() {
+
+
+}).call(this);
