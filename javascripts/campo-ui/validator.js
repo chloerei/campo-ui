acc7af8420a9960e407c05e5cf5e6efca5cd2752
Rei <chloerei@gmail.com>
2014-08-03 00:03:17 +0800

    Automated commit at 2014-08-02 16:03:17 UTC by middleman-deploy 0.2.4

--- javascripts/campo-ui/validator.js
+++ javascripts/campo-ui/validator.js
@@ -1,14 +1,4 @@
 (function() {
-  $(document).on('click', '.left-nav .left-nav-toggle', function() {
-    $(this).closest('.left-nav').addClass('active');
-    return $('body').addClass('noscroll');
-  }).on('click', '.left-nav .left-nav-mask', function() {
-    $(this).closest('.left-nav').removeClass('active');
-    return $('body').removeClass('noscroll');
-  });
-
-}).call(this);
-(function() {
   var Validator;
 
   Validator = (function() {
@@ -253,9 +243,5 @@
   $(function() {
     return $('form:not([novalidate])').validate();
   });
-
-}).call(this);
-(function() {
-
 
 }).call(this);
