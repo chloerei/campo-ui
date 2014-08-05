2baf7fc6d3ddc7bb53a2f2461608d145ea2e8298
Rei <chloerei@gmail.com>
2014-08-05 20:55:45 +0800

    Automated commit at 2014-08-05 12:55:45 UTC by middleman-deploy 0.2.4

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,6 +1,11 @@
 (function() {
-  $(document).on('click', '.left-nav .left-nav-toggle', function() {
-    $(this).closest('.left-nav').addClass('active');
+  $(document).on('click', '[data-toggle=left-nav]', function() {
+    var leftNav;
+    leftNav = $($(this).data('target'));
+    if (!leftNav.find('.left-nav-mask').length) {
+      leftNav.prepend($('<div class="left-nav-mask"></div>'));
+    }
+    leftNav.addClass('active');
     return $('body').addClass('noscroll');
   }).on('click', '.left-nav .left-nav-mask', function() {
     $(this).closest('.left-nav').removeClass('active');
